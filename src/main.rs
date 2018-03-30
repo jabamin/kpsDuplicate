7d19a7e38e9c8a82243c02c022ace796c3d078dd
jabamin <jabamin2013@gmail.com>
2018-03-30 16:55:15 +0200

    Usage of serde

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-extern crate rustc_serialize;
-use rustc_serialize::json::Json;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 use std::fs::File;
 use std::io::Read;
+use std::error::Error;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
+struct Portalapiaccessstore {
+    storeId:     String,
+    typeId: String,
+    objects: Vec<Object>,
+}
+#[derive(Deserialize, Debug)]
+struct Object {
+    organizationId:     String,
+    createdBy: String,
+    entityId: String,
+    id: String,
+    createdOn:  i64,
+    apiId:  String,
+    enabled:    bool,
+}
+
+fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Portalapiaccessstore, Box<Error>> {
+    // Open the file in read-only mode.
+    let file = File::open(path)?;
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(file)?;
+
+    // Return the `User`.
+    Ok(u)
+}
+
 
 fn main() {
-    let mut file = File::open("pages.json4").unwrap();
-    let mut data = String::new();
-    file.read_to_string(&mut data).unwrap();
-
-    let json = Json::from_str(&data).unwrap();
-    println!("{}", json.find_path(&["objects", "entityId"]).unwrap());
-    println!("{}", json.find_path(&["objects", "apiId"]).unwrap());
-    println!("{}", json );
+    let u = read_user_from_file("pages.json4").unwrap();
+    println!("{:#?}", u);
 }
